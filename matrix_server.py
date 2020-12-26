527039a2171326b69d90f31a481f38380ee088c1
Bjoern <btestacc@gmail.com>
2020-12-26 20:23:37 +0100

    Changed servername to server_name Added server_url

--- matrix_server.py
+++ matrix_server.py
@@ -3,18 +3,19 @@ import urllib.parse
 import json
 
 class Matrix_server():
-    def __init__(self,servername =""):
-        self.servername = servername
+    def __init__(self,server_name =""):
+        self.server_name = server_name
         self.header=""
 
     def load_config(self,filename = ""):
         if filename == "":
-            filename=self.servername.strip('https://')+'.json'
+            filename=self.server_name+'.json'
         try:
             file = open(filename)
             self.auth_user = json.load(file)
             self.set_access_token(self.auth_user['access_token'])
-            self.servername="https://"+self.auth_user['home_server']
+            self.server_name=self.auth_user['home_server']
+            self.server_url='https://'+self.server_name
             file.close()
             return True
         except:
@@ -22,14 +23,14 @@ class Matrix_server():
 
     def save_config(self,filename = ""):
         if filename == "":
-            filename=self.servername.strip('https://')+".json"
+            filename=self.server_name+".json"
         with open(filename,'w') as file:
             json.dump(self.auth_user,fp=file)
             
 
     def login(self,username,password, saveconfig=False,filename = ""):
         self.username=username
-        adress= self.servername+"/_matrix/client/r0/login"
+        adress= self.server_url+"/_matrix/client/r0/login"
         data = json.dumps({"user" : username, "password" : password, "type":"m.login.password"})
         try: 
             r = requests.post(adress, data)
@@ -47,7 +48,7 @@ class Matrix_server():
         self.header = {"Authorization":"Bearer " + self.access_token}
             
     def get_room_id_from_alias(self, room_alias):
-        url=self.servername+f"/_synapse/client/r0/directory/room/{urllib.parse.quote(room_alias)}"
+        url=self.server_url+f"/_synapse/client/r0/directory/room/{urllib.parse.quote(room_alias)}"
         res = requests.get(url,headers = self.header)
         if res.status_code==200:
                 return res.json()['room_id']
@@ -55,7 +56,7 @@ class Matrix_server():
             return None
 
     def get_users_from_room(self, room_id):
-        url=self.servername+f"/_snyapse/client/r0/rooms/{urllib.parse.quote(room_id)}/joined_members"
+        url=self.server_url+f"/_snyapse/client/r0/rooms/{urllib.parse.quote(room_id)}/joined_members"
         res = requests.get(url,headers=self.header)
         if res.status_code==200:
                 return res.json()
@@ -63,7 +64,7 @@ class Matrix_server():
             return None
 
     def get_rooms(self):
-        url=self.servername+"/_synapse/admin/v1/rooms"
+        url=self.server_url+"/_synapse/admin/v1/rooms"
         res = requests.get(url,headers=self.header)
         if res.status_code==200:
                 return res.json()['rooms']
@@ -71,7 +72,7 @@ class Matrix_server():
             return None
 
     def get_users(self):
-        url=self.servername+"/_synapse/admin/v2/users"
+        url=self.server_url+"/_synapse/admin/v2/users"
         res = requests.get(url,headers=self.header)
         if res.status_code==200:
             return res.json()['users']
@@ -79,7 +80,7 @@ class Matrix_server():
             return None
 
     def delete_room(self, room_id):
-        url=self.servername+f"/_synapse/admin/v1/rooms/{room_id}/delete"
+        url=self.server_url+f"/_synapse/admin/v1/rooms/{room_id}/delete"
         res = requests.post(url,headers=self.header)
         if res.status_code==200:
               return res.json()
@@ -87,7 +88,7 @@ class Matrix_server():
             return None
 
     def new_user(self, user_data):
-        url=self.servername+f"/_synapse/admin/v2/users/{urllib.parse.quote(user_data['user_id'])}"
+        url=self.server_url+f"/_synapse/admin/v2/users/{urllib.parse.quote(user_data['user_id'])}"
         data = json.dumps({"password" : user_data['password'], "displayname" : user_data['displayname']})
         res = requests.put(url, data = data, headers=self.header)
         if res.status_code==200:
@@ -95,7 +96,7 @@ class Matrix_server():
         return None
     
     def deactivate_user(self, user_id):
-        url=self.servername+f"/_synapse/admin/v1/deactivate/{urllib.parse.quote(user_id)}"
+        url=self.server_url+f"/_synapse/admin/v1/deactivate/{urllib.parse.quote(user_id)}"
         data = json.dumps({"erase": True})
         res = requests.post(url, data, headers=self.header)
         if res.status_code==200:
@@ -103,7 +104,7 @@ class Matrix_server():
         return None        
 
     def get_user(self, user_id):
-        url=self.servername+f"/_synapse/admin/v2/users/{urllib.parse.quote(user_id)}"
+        url=self.server_url+f"/_synapse/admin/v2/users/{urllib.parse.quote(user_id)}"
         res = requests.get(url, headers=self.header)
         if res.status_code==200:
             return res.json()
@@ -111,7 +112,7 @@ class Matrix_server():
             return None
 
     def reset_password(self,user_id,password, logout_devices = True):
-        url=self.servername+f"/_synapse/admin/v1/reset_password/{urllib.parse.quote(user_id)}"
+        url=self.server_url+f"/_synapse/admin/v1/reset_password/{urllib.parse.quote(user_id)}"
         data = json.dumps({"new_password":password, "logout_devices":logout_devices})
         res = requests.post(url, data, headers=self.header)
         if res.status_code==200:
@@ -120,7 +121,7 @@ class Matrix_server():
             return None
 
     def get_joined_rooms(self, user_id):
-        url=self.servername+f"/_synapse/admin/v1/users/{urllib.parse.quote(user_id)}/joined_rooms"
+        url=self.server_url+f"/_synapse/admin/v1/users/{urllib.parse.quote(user_id)}/joined_rooms"
         res = requests.get(url, headers=self.header)
         if res.status_code==200:
             return res.json()
@@ -128,7 +129,7 @@ class Matrix_server():
             return None
 
     def get_user_devices(self,user_id):
-        url=self.servername+f"/_synapse/admin/v2/users/{urllib.parse.quote(user_id)}/devices"
+        url=self.server_url+f"/_synapse/admin/v2/users/{urllib.parse.quote(user_id)}/devices"
         res = requests.get(url, headers=self.header)
         if res.status_code==200:
             return res.json()
