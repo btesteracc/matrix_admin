c869bc1520a874004dfebae74ed43c88d57291ae
bjoern <btestacc@gmail.com>
2020-12-27 15:14:54 +0100

    Added mutually_exclussive_group

--- matrix_admin_cli.py
+++ matrix_admin_cli.py
@@ -8,9 +8,13 @@ import re
 
 parser = argparse.ArgumentParser(prog="matrix_admin_cli.py", description="A simple Matrix-Server admin tool")
 
-parser.add_argument('-l','--login',help="Login manually",action="store_true")
+login_group = parser.add_mutually_exclusive_group()
+login_group.add_argument('-l','--login',help="Login manually",action="store_true")
+login_group.add_argument('-c','--config_file',help='load config from file',type=argparse.FileType('r'))
+login_group.add_argument('-ld','--login_data',help="Login with given data",action="store_true")
 
-# sub_parser=parser.add_subparsers(help='Sub command login_config')
+
+#login_parser argparse.ArgumentParser()
 # login_parser = sub_parser.add_parser("--login_config", help="Login config on commandline")
 # login_parser.add_argument('-H','--host',help='hostname')
 # login_parser.add_argument('-u','--user',help='username of the server admin')
@@ -22,7 +26,7 @@ parser.add_argument('-au','--add_user',help='create new user - asks for username
 parser.add_argument('-du','--delete_user',help='delete user with given userid (@user:servername)')
 parser.add_argument('-dr','--delete_room',help='delete rom with given roomid')
 parser.add_argument('-cp','--change_password',help='change password for given userid')
-parser.add_argument('-c','--config_file',help='load config from file',type=argparse.FileType('r'))
+
 parser.add_argument('-lu','--list_users',help='list users from server')
 parser.add_argument('-lr','--list_rooms',help='list rooms from server',action="store_true")
 parser.add_argument('-q','--quiet', help='No comments, just JSON Output',action="store_true")
